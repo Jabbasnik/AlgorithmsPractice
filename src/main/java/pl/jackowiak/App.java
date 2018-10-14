184a02cff9763d49fd1e032eaafc1b9b1e3f2c8a
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2018-10-14 12:13:04 +0200

    Initial commit - TwoToOne algorithm

--- src/main/java/pl/jackowiak/App.java
+++ src/main/java/pl/jackowiak/App.java
@@ -1,13 +1,14 @@
 package pl.jackowiak;
 
 /**
- * Hello world!
+ * Practice repository
+ * Created by Marcin Jackowiak
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello! Good day for practice!" );
     }
 }
---
+++ src/main/java/pl/jackowiak/TwoToOne/TwoToOne.java
@@ -0,0 +1,8 @@
+package pl.jackowiak.TwoToOne;
+
+public class TwoToOne {
+
+
+
+
+}
