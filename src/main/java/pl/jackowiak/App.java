27f36c0fff77ce56e76a3197df39eacfe47fcf5e
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2018-10-25 16:37:02 +0200

    Diamond kata finished

--- src/main/java/pl/jackowiak/App.java
+++ src/main/java/pl/jackowiak/App.java
@@ -1,6 +1,7 @@
 package pl.jackowiak;
 
 import pl.jackowiak.expandnumber.ExpandNumber;
+import pl.jackowiak.giveMeADiamond.GivemeADiamond;
 import pl.jackowiak.isogram.Isogram;
 import pl.jackowiak.maximumSubarraySum.MaxSubarraySum;
 import pl.jackowiak.mumbling.Mumbling;
@@ -29,6 +30,8 @@ public class App {
         System.out.println("Max subbarary | " + MaxSubarraySum.sequence(new int[]{-21, -12, -16, -14, 11, -16, 29, -2, -2, 1, -30, 26, -2, 28, -1, 4, -19, -28, -9, 4, -15, 15, -13, 13, -25, 11, 18, 16, -10, 12, -16, 17, 3, -21, 17, -20, 27, 22, -12, 22, -7, 13, 14, 7, 3, 25, 16, 9, -30, 25}));
 
         System.out.println("Triple problem | " + TripleTrouble.TripleDouble(451999277L, 41177722899L));
+
+        System.out.println("Give me a diamond | \n" + GivemeADiamond.print(7));
 
     }
 }
--- src/main/java/pl/jackowiak/giveMeADiamond/GivemeADiamond.java
+++ src/main/java/pl/jackowiak/giveMeADiamond/GivemeADiamond.java
@@ -3,9 +3,32 @@ package pl.jackowiak.giveMeADiamond;
 public class GivemeADiamond {
 
     public static String print(int n) {
-        // TODO your code here
+        String diamond = "";
+        if (n != 0 && n % 2 != 0 && n > 0) {
+            for (int i = 1; i < n + 1; i += 2) {
+                for (int j = 0; j < (n - i) / 2; j++) {
+                    diamond = diamond.concat(" ");
+                }
+                for (int j = 0; j < i; j++) {
+                    diamond = diamond.concat("*");
+                }
+                diamond = diamond.concat("\n");
+            }
+
+            for (int i = n - 2; i > 0; i -= 2) {
+                for (int j = 0; j < (n - i) / 2; j++) {
+                    diamond = diamond.concat(" ");
+                }
+
+                for (int j = 0; j < i; j++) {
+                    diamond = diamond.concat("*");
+                }
+                diamond = diamond.concat("\n");
+            }
+
+            return diamond;
+        }
 
         return null;
     }
-
 }
